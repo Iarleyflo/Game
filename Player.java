f4e6d994707e448f516963d9fafaffbcd0d3ff2b
Luyen3333 <143017664+Luyen3333@users.noreply.github.com>
2023-11-29 11:54:23 -0600

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,5 +1,3 @@
-
-
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -9,55 +7,62 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+public class Player extends GameObject implements KeyListener {
+    private BufferedImage image;
+    private int speed = 20; // Adjust the speed as needed
 
-public class Player extends GameObject implements KeyListener{
-	private BufferedImage image;
-	
-	public Player() {
-		this.setX(320);
-		this.setY(300);
-		this.setHeight(150);
-		this.setWidth(72);
-		loadImage();
-	}
-	
-	private void loadImage() {
-		File file = new File("spacecraft.png");
-		try {
-			image = ImageIO.read(file);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public Player() {
+        this.setX(320);
+        this.setY(300);
+        this.setHeight(150);
+        this.setWidth(72);
+        loadImage();
+    }
 
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-		
-	}
+    private void loadImage() {
+        File file = new File("spacecraft.png");
+        try {
+            image = ImageIO.read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public void render(Graphics g) {
-		g.drawImage(this.image, this.getX(), this.getY(), this.getWidth(),this.getHeight(), null);
-	}
+    @Override
+    public void update() {
+        // TODO: Implement movement logic here if needed
+    }
 
-	@Override
-	public void keyPressed(KeyEvent keyEvent) {
-		System.out.println("KeyCode: " + keyEvent.getKeyCode());
-	}
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(this.image, this.getX(), this.getY(), this.getWidth(), this.getHeight(), null);
+    }
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        int keyCode = keyEvent.getKeyCode();
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+        if (keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_W) {
+            this.setY(this.getY() - speed);
+        } 
+        else if (keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_S) {
+            this.setY(this.getY() + speed);
+        } 
+        else if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_A) {
+            this.setX(this.getX() - speed);
+        } 
+        else if (keyCode == KeyEvent.VK_RIGHT || keyCode == KeyEvent.VK_D) {
+            this.setX(this.getX() + speed);
+        }
+    }
 
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO: Implement if needed
+    }
 
-
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO: Implement if needed
+    }
 }
