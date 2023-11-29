2f6ac1ac70c5465ab81cbd696c988f21cc22f51a
Iarleyflo <143017895+Iarleyflo@users.noreply.github.com>
2023-11-29 12:15:41 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,18 +4,22 @@ import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
 public class Player extends GameObject implements KeyListener {
     private BufferedImage image;
     private int speed = 20; // Adjust the speed as needed
+    private List<Projectile> projectiles;
 
     public Player() {
         this.setX(320);
         this.setY(300);
         this.setHeight(150);
         this.setWidth(72);
+        this.projectiles = new ArrayList<>();
         loadImage();
     }
 
@@ -30,12 +34,24 @@ public class Player extends GameObject implements KeyListener {
 
     @Override
     public void update() {
-        // TODO: Implement movement logic here if needed
+        // Update existing projectiles
+        for (int i = 0; i < projectiles.size(); i++) {
+            Projectile projectile = projectiles.get(i);
+            projectile.update();
+            // Remove projectiles that are out of bounds
+            if (projectile.getX() < 0 || projectile.getX() > Game.WIDTH || projectile.getY() < 0 || projectile.getY() > Game.HEIGHT) {
+                projectiles.remove(i);
+            }
+        }
     }
 
     @Override
     public void render(Graphics g) {
         g.drawImage(this.image, this.getX(), this.getY(), this.getWidth(), this.getHeight(), null);
+        // Render existing projectiles
+        for (Projectile projectile : projectiles) {
+            projectile.render(g);
+        }
     }
 
     @Override
@@ -44,25 +60,26 @@ public class Player extends GameObject implements KeyListener {
 
         if (keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_W) {
             this.setY(this.getY() - speed);
-        } 
-        else if (keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_S) {
+        } else if (keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_S) {
             this.setY(this.getY() + speed);
-        } 
-        else if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_A) {
+        } else if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_A) {
             this.setX(this.getX() - speed);
-        } 
-        else if (keyCode == KeyEvent.VK_RIGHT || keyCode == KeyEvent.VK_D) {
+        } else if (keyCode == KeyEvent.VK_RIGHT || keyCode == KeyEvent.VK_D) {
             this.setX(this.getX() + speed);
+        } else if (keyCode == KeyEvent.VK_SPACE) {
+            // Create a new projectile and add it to the list
+            Projectile newProjectile = new Projectile(this.getX(), this.getY());
+            projectiles.add(newProjectile);
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        // TODO: Implement if needed
+        // Implement if needed
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-        // TODO: Implement if needed
+        // Implement if needed
     }
 }
